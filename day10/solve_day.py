e024e71726d5c02a7aad8d68542f059c9f077b91
OliverOA <OliverAOakman@gmail.com>
2022-12-12 22:21:42 +1100

    Refactor and solve day 9

--- day10/solve_day.py
+++ day10/solve_day.py
@@ -3,19 +3,27 @@ from typing import List
 from solver import Solver
 
 
-class DayX(Solver):
+class Day10(Solver):
     def __init__(self, day: int, use_sample: bool) -> None:
         super().__init__(use_sample)
         self.my_base_path = __file__
         self.day = day
 
     def part1(self, data: List) -> None:
-        pass
+        register = 1
+        action_cycles = [] * len(data)
+        print(action_cycles)
+        for instruction in data:
+            if instruction.startswith("addx"):
+                value_to_add = int(instruction.split(" "))
+
+            elif instruction == "noop":
+                pass
 
     def part2(self, data: List) -> None:
         pass
 
 
 def solve_day(day: int, use_sample: bool):
-    solver = DayX(day, use_sample)
+    solver = Day10(day, use_sample)
     solver.solve()
--- day9/solve_day.py
+++ day9/solve_day.py
@@ -1,7 +1,7 @@
-import time
 from typing import List
 
 import numpy as np
+import sys
 
 from solver import Solver
 
@@ -16,15 +16,28 @@ class Day9(Solver):
             "D": [0, -1],
             "L": [-1, 0],
             "R": [1, 0],
+            "": [0, 0],
         }
 
-    def _is_snap_rope_required(self, head_pos: List, tail_pos: List) -> bool:
-        # True if rope needs to be snapped
-        snap_required = abs(head_pos[0] - tail_pos[0]) > 1 or abs(head_pos[1] - tail_pos[1]) > 1
-        return snap_required
+    def _move_link_direction(self, parent_link: List, child_link: List) -> List[int]:
+        horiz_delta = parent_link[0] - child_link[0]
+        vert_delta = parent_link[1] - child_link[1]
 
-    def _get_rope_direction_bias(self, parent_pos: List, child_pos: List) -> List[int]:
-        pass
+        if abs(horiz_delta) <= 1 and abs(vert_delta) <= 1:
+            return
+        else:
+            horiz_move = 0 if horiz_delta == 0 else int(horiz_delta / abs(horiz_delta))
+            vert_move = 0 if vert_delta == 0 else int(vert_delta / abs(vert_delta))
+            return [horiz_move, vert_move]
+
+    def _visualise(self, links: List, grid_size: int) -> None:
+        np.set_printoptions(threshold=sys.maxsize, linewidth=10 * grid_size)
+        grid = -1 * np.ones((grid_size, grid_size))
+
+        for idx, link in enumerate(links):
+            grid[link[0] + grid_size // 2, link[1] + grid_size // 2] = idx
+
+        print(np.array_str(np.rot90(grid)).replace("-1", ".."))
 
     def part1(self, data: List) -> None:
         pos_h = [0, 0]
@@ -39,40 +52,36 @@ class Day9(Solver):
             direction_vector = self.DIRECTIONS[direction]
 
             for _ in range(num):
-                lag_pos_h = pos_h.copy()
                 pos_h = [sum(i) for i in zip(pos_h, direction_vector)]
-                if self._is_snap_rope_required(pos_h, pos_t):
-                    pos_t = lag_pos_h
+                new_t_dir = self._move_link_direction(pos_h, pos_t)
+                if new_t_dir is not None:
+                    pos_t = [sum(i) for i in zip(pos_t, new_t_dir)]
                     all_pos_t.add(",".join([str(x) for x in pos_t]))
+
         return len(all_pos_t)
 
     def part2(self, data: List) -> None:
-        links = [[0, 0]] * 4
+        ROPE_LENGTH = 10
+        GRID_SIZE = 32
+        links = [[0, 0]] * ROPE_LENGTH
 
         all_pos_t = {",".join([str(x) for x in [0, 0]])}
-
+        all_pos_order = []
         for instruction in data:
             direction = instruction.split(" ")[0]
             num = int(instruction.split(" ")[1])
 
             direction_vector = self.DIRECTIONS[direction]
 
-            # TODO FIX THE LOGIC TO PULL THE ROPE IN RATHER THAN DRAG
             for _ in range(num):
-                parent_link_prev = links[0].copy()
                 links[0] = [sum(i) for i in zip(links[0], direction_vector)]
-                print("Moved", links[0])
 
                 for idx in range(1, len(links)):
-                    if self._is_snap_rope_required(links[idx - 1], links[idx]):
-                        prev_location = links[idx].copy()
-                        links[idx] = parent_link_prev
-                        parent_link_prev = prev_location.copy()
-                        print(f"Update: {idx}", links)
-
-                print("Step finished")
+                    link_change_required = self._move_link_direction(links[idx - 1], links[idx])
+                    if link_change_required is not None:
+                        links[idx] = [sum(i) for i in zip(links[idx], link_change_required)]
                 all_pos_t.add(",".join([str(x) for x in links[-1]]))
-            print("Instruction finished")
+                all_pos_order.append(links[-1])
 
         return len(all_pos_t)
 
