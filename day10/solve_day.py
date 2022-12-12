1c965d791c9408b826fec44e715361ad10aeb7a9
OliverOA <OliverAOakman@gmail.com>
2022-12-12 23:09:19 +1100

    Day 10 solved

--- day10/solve_day.py
+++ day10/solve_day.py
@@ -9,8 +9,7 @@ class Day10(Solver):
         self.my_base_path = __file__
         self.day = day
 
-    def part1(self, data: List) -> None:
-        register = 1
+    def _build_action_register(self, data: List) -> List:
         action_cycles = []
 
         for idx, instruction in enumerate(data):
@@ -22,9 +21,14 @@ class Day10(Solver):
             elif instruction == "noop":
                 action_cycles.append(0)
 
+        return action_cycles
+
+    def part1(self, data: List) -> int:
+        action_cycles = self._build_action_register(data)
+
+        register = 1
         signal_strengths = []
         signal_cycles = list(range(20, len(action_cycles), 40))
-        print("Signal cycles", signal_cycles)
 
         for idx, val in enumerate(action_cycles):
             actual_idx = idx + 1
@@ -34,8 +38,34 @@ class Day10(Solver):
 
         return sum(signal_strengths)
 
-    def part2(self, data: List) -> None:
-        pass
+    def part2(self, data: List) -> str:
+        action_cycles = self._build_action_register(data)
+
+        # Initialise all of the parameters we will use
+        crt_rows = []
+        curr_row = ""
+        curr_col = 0
+        row_num = 0
+        ROW_WIDTH = 40
+        sprite_pos = 1
+
+        for idx, val in enumerate(action_cycles):
+            curr_col = idx - (ROW_WIDTH * row_num)
+            if abs(curr_col - sprite_pos) <= 1:
+                curr_row += "\u2588"
+            else:
+                curr_row += " "
+
+            if (idx + 1) % ROW_WIDTH == 0 and idx > 1:
+                crt_rows.append(curr_row)
+                row_num += 1
+                curr_row = ""
+            sprite_pos += val
+
+        for row in crt_rows:
+            print(row)
+
+        return "ABOVE"
 
 
 def solve_day(day: int, use_sample: bool):
