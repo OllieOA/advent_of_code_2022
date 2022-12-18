353baacd2e31dbe0a553997f22e50b37ad79cc78
OliverOA <OliverAOakman@gmail.com>
2022-12-18 22:03:41 +1100

    Part 1 solved. Optimising

--- day17/solve_day.py
+++ day17/solve_day.py
@@ -16,6 +16,8 @@ SHAPES = [
 
 SHAPE_HEIGHTS = [1, 3, 3, 4, 2]
 
+INSTRUCTIONS = {"<": -1, ">": 1}
+
 
 class Day17(Solver):
     def __init__(self, day: int, use_sample: bool) -> None:
@@ -36,16 +38,18 @@ class Day17(Solver):
                 row_str += LOOKUP[element]
             print(row_str)
 
-    def part1(self, data: List) -> None:
-        instructions = data[0]
-
-        grid = np.ones((7, 1)) * 2
+    def _run_simulation(self, instructions: str, number_of_runs: int) -> int:
+        TREADMILL_SIZE = 500
+        grid = np.zeros((7, TREADMILL_SIZE)) * 2
+        grid[:, 0] = 2
+        # self._visualise(grid)
 
         highest_rock_level = 1
+        cumulated_base_height = 0
         instruction_count = 0
 
         rock_rested = True
-        for ridx in tqdm(range(2023)):
+        for ridx in tqdm(range(number_of_runs)):
             # if no rock, spawn rock
             if rock_rested:
                 # print("SPAWNING ROCK")
@@ -53,12 +57,16 @@ class Day17(Solver):
                 rock_idx_to_spawn = ridx % 5
 
                 # Pad if needed
-                if (grid.shape[1] - highest_rock_level) < SHAPE_HEIGHTS[rock_idx_to_spawn] + 3:
-                    new_padding = np.zeros((7, SHAPE_HEIGHTS[rock_idx_to_spawn] + 3))
-                    grid = np.concatenate([grid, new_padding], axis=1)
-                rock_shape = SHAPES[rock_idx_to_spawn]
+                if grid.shape[1] < highest_rock_level + 3 + SHAPE_HEIGHTS[rock_idx_to_spawn]:
+                    new_padding = np.zeros((7, TREADMILL_SIZE - 50))
+                    # print("GRABBING", grid[:, 100:].shape)
+                    grid = np.concatenate([grid[:, TREADMILL_SIZE - 50 :], new_padding], axis=1)
+                    cumulated_base_height += TREADMILL_SIZE - 50
+                    highest_rock_level -= TREADMILL_SIZE - 50
 
+                rock_shape = SHAPES[rock_idx_to_spawn]
                 current_rock = (2, highest_rock_level + 3)
+
                 for coord in rock_shape:
                     grid[coord[0] + current_rock[0], coord[1] + current_rock[1]] = 1
 
@@ -70,79 +78,71 @@ class Day17(Solver):
 
                 # print("INSTRUCTION TO DO", instruction)
 
-                modifier = 1
-                if instruction == "<":
-                    modifier = -1
+                modifier = INSTRUCTIONS[instruction]
 
                 # Check if all elements can move sideways
-                moveable_sideways = []
+                moveable_sideways = True
                 for coord in rock_shape:
                     curr_coord = (current_rock[0] + coord[0], current_rock[1] + coord[1])
                     if curr_coord[0] + modifier < 0 or curr_coord[0] + modifier == 7:
-                        moveable_sideways.append(False)
-                    elif grid[curr_coord[0] + modifier, curr_coord[1]] != 2:
-                        moveable_sideways.append(True)
-                    else:
-                        moveable_sideways.append(False)
+                        moveable_sideways = False
+                        break
+                    elif grid[curr_coord[0] + modifier, curr_coord[1]] == 2:
+                        moveable_sideways = False
+                        break
 
-                if all(moveable_sideways):
-                    new_base_coord = (current_rock[0] + modifier, current_rock[1])
-                    for coord in rock_shape:
-                        curr_coord = (current_rock[0] + coord[0], current_rock[1] + coord[1])
-                        grid[curr_coord[0], curr_coord[1]] = 0
-
-                    current_rock = new_base_coord
+                if moveable_sideways:
+                    current_rock = (current_rock[0] + modifier, current_rock[1])
+                    grid[grid == 1] = 0
 
                     for coord in rock_shape:
                         curr_coord = (current_rock[0] + coord[0], current_rock[1] + coord[1])
                         grid[curr_coord[0], curr_coord[1]] = 1
 
                 # Check if can move down
-                moveable_down = []
+                moveable_down = True
                 for coord in rock_shape:
                     curr_coord = (current_rock[0] + coord[0], current_rock[1] + coord[1])
-                    if grid[curr_coord[0], curr_coord[1] - 1] != 2:
-                        moveable_down.append(True)
-                    else:
-                        moveable_down.append(False)
+                    if grid[curr_coord[0], curr_coord[1] - 1] == 2:
+                        moveable_down = False
                         break
 
                 # print("MOVABLE SIDEWAYS?", moveable_sideways)
                 # print("MOVABLE DOWN?", moveable_down)
 
-                if all(moveable_down):
-                    new_base_coord = (current_rock[0], current_rock[1] - 1)
-                    for coord in rock_shape:
-                        curr_coord = (current_rock[0] + coord[0], current_rock[1] + coord[1])
-                        grid[curr_coord[0], curr_coord[1]] = 0
-
-                    current_rock = new_base_coord
+                if moveable_down:
+                    current_rock = (current_rock[0], current_rock[1] - 1)
+                    grid[grid == 1] = 0
 
                     for coord in rock_shape:
                         curr_coord = (current_rock[0] + coord[0], current_rock[1] + coord[1])
                         grid[curr_coord[0], curr_coord[1]] = 1
 
                 else:
-                    for coord in rock_shape:
-                        curr_coord = (current_rock[0] + coord[0], current_rock[1] + coord[1])
-                        grid[curr_coord[0], curr_coord[1]] = 2  # Now stationary
+                    grid[grid == 1] = 2  # Now stationary
 
                     rock_rested = True
                     # print("ROCK RESTED")
+                    # self._visualise(grid)
+                    # print("====")
 
                     heights = []
                     for coord in rock_shape:
                         heights.append(current_rock[1] + coord[1])
 
-                    highest_rock_level = max(heights) + 1
+                    highest_rock_level = max(highest_rock_level, max(heights) + 1)
                     # print("SETTING NEW HEIGHT TO", highest_rock_level)
 
                 # time.sleep(0.5)
+        return cumulated_base_height + highest_rock_level - 1
 
-        return highest_rock_level
+    def part1(self, data: List) -> None:
+        instructions = data[0]
+        return self._run_simulation(instructions, 2022)
 
     def part2(self, data: List) -> None:
-        pass
+        instructions = data[0]
+        return self._run_simulation(instructions, 1000000000000)
 
 
 def solve_day(day: int, use_sample: bool):
