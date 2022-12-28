18ecf1db4a8753f1ae250d749540d997f54e034b
OliverOA <OliverAOakman@gmail.com>
2022-12-29 00:18:09 +1100

    Progress on 15. No solve

--- day15/solve_day.py
+++ day15/solve_day.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
-import itertools
 import re
+from tqdm import tqdm
 from typing import List, Tuple, Dict
 
 import numpy as np
@@ -34,13 +34,12 @@ class Day15(Solver):
     def _build_area(
         self, intervals: Dict[int, List], starting_coords: List, index: int, direction: int
     ) -> None:
-        sub_interval = deepcopy(starting_coords)
         if index not in intervals:
             intervals[index] = []
 
         converged = False
-        curr_min = sub_interval[0]
-        curr_max = sub_interval[1]
+        curr_min = starting_coords[0]
+        curr_max = starting_coords[1]
         while not converged:
             curr_min += 1
             curr_max -= 1
@@ -52,69 +51,47 @@ class Day15(Solver):
             intervals[index].append([curr_min, curr_max])
 
     def _consolidate_ranges(self, intervals: Dict[int, List]) -> None:
-        for row, ranges in intervals.items():
-            consolidated_ranges = [tuple(x) for x in ranges]
+        for row, ranges in tqdm(intervals.items()):
+            consolidated_ranges = sorted([tuple(x) for x in ranges])
             all_consolidated = False
 
+            loops = 0
             while not all_consolidated:
-                print("OPERATION BEGIN")
+                loops += 1
                 all_consolidated = True
-                if len(consolidated_ranges) < 2:
+                if len(consolidated_ranges) == 1:
                     break
-                all_combos = itertools.combinations(consolidated_ranges, 2)
-                new_ranges = []
-                removed = []
-                for combo in all_combos:
-                    # print("COMBO IS", combo)
+
+                new_ranges = set([])
+                for idx in range(len(consolidated_ranges) - 1):
+                    combo = (consolidated_ranges[idx], consolidated_ranges[idx + 1])
+                    all_nums = [combo[0][0], combo[0][1], combo[1][0], combo[1][1]]
+
                     first_range = set(range(combo[0][0], combo[0][1] + 1))
                     second_range = set(range(combo[1][0], combo[1][1] + 1))
 
-                    if len(first_range.intersection(second_range)):  # Overlap found, consolidate
+                    if len(first_range.intersection(second_range)) > 0:
                         all_consolidated = False
-                        new_combo = (min(combo[0] + combo[1]), max(combo[0] + combo[1]))
-                        print("OVERLAP! Attempting to add", new_combo, "to", new_ranges)
-                        removed.extend([combo[0], combo[1]])
-
-                        if new_combo not in new_ranges:
-                            new_ranges.append(new_combo)
-                            # print("Updated new_ranges", new_ranges)
-
+                        # Intersection
+                        new_ranges.add((min(all_nums), max(all_nums)))
+                        break
+                    elif abs(combo[0][1] - combo[1][0]) == 1:  # 1 away
+                        all_consolidated = False
+                        new_ranges.add((min(all_nums), max(all_nums)))
+                        break
                     else:
-                        if combo[0] not in new_ranges and combo[0] not in removed:
-                            new_ranges.append(combo[0])
-                        if combo[1] not in new_ranges and combo[1] not in removed:
-                            new_ranges.append(combo[1])
+                        new_ranges.add(combo[0])
+                        if idx == len(consolidated_ranges) - 1:
+                            new_ranges.add(combo[1])
 
-                        print("NO OVERLAP!", new_ranges)
+                if (idx + 2) < len(consolidated_ranges):  # Early exit
+                    new_ranges = new_ranges.union(set(consolidated_ranges[idx + 2 :]))
+                consolidated_ranges = sorted(list(new_ranges))
 
-                consolidated_ranges = new_ranges
-
-            print("FINAL CONSOLIDATED RANGE", consolidated_ranges)
-            # raise Exception
             intervals[row] = consolidated_ranges
 
-            # Generate all pairs of ranges
-
-        # for row, ranges in intervals.items():
-        #     all_ranges = [list(range(x[0], x[1] + 1)) for x in ranges]
-        #     all_nums = sorted(list(set([x for sublist in all_ranges for x in sublist])))
-
-        #     curr_min = all_nums[0]
-        #     prev_ele = curr_min
-        #     consolidated_ranges = []
-        #     for num in all_nums[1:]:
-        #         if num - prev_ele != 1:
-        #             consolidated_ranges.append((curr_min, prev_ele))
-        #             curr_min = num
-        #             prev_ele = curr_min
-        #         prev_ele = num
-
-        #     # Append last range (or only range)
-        #     consolidated_ranges.append((curr_min, prev_ele))
-
-        #     intervals[row] = consolidated_ranges
-
     def part1(self, data: List[str]) -> None:
+        print("Building object for processing...")
         sensors_to_beacons = {}
         min_x = 10e10
         min_y = 10e10
@@ -146,7 +123,7 @@ class Day15(Solver):
         # Go over each of the radii and build an interval for each row, then
         # consolidate. Form is {row: [(col1, col2),  (col3, col4) ...]}
         blocked_intervals = {}  # Only build this on the x axis
-        for sensor_coord, sensor_radius in sensor_radii.items():
+        for sensor_coord, sensor_radius in tqdm(sensor_radii.items()):
             base_col_idx, base_row_idx = sensor_coord
             if base_row_idx not in blocked_intervals:
                 blocked_intervals[base_row_idx] = []
